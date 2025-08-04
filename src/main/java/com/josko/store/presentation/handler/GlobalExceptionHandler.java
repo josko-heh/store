f5879f01c2a255e011d230242df82c37790914cc
josko <jklo.jurce@gmail.com>
2025-08-04 23:43:31 +0200

    add ValidationError

--- src/main/java/com/josko/store/presentation/handler/GlobalExceptionHandler.java
+++ src/main/java/com/josko/store/presentation/handler/GlobalExceptionHandler.java
@@ -1,32 +1,59 @@
 package com.josko.store.presentation.handler;
 
+import com.josko.store.presentation.dto.ValidationError;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.stream.Collectors;
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
 	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<String> handleValidationException(ConstraintViolationException ex) {
-
+	public ResponseEntity<ValidationError> handleValidationException(ConstraintViolationException ex) {
+		
 		var errors = ex.getConstraintViolations().stream()
 				.map(v -> v.getPropertyPath() + ": " + v.getMessage())
-				.collect(Collectors.joining("; "));
+				.toList();
 
-		return ResponseEntity.badRequest().body("Validation failed: " + errors);
+		return ResponseEntity.badRequest().body(buildValidationError(errors, "Validation failed."));
 	}
-	
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<String> handleValidationException(DataIntegrityViolationException ex) {
 
-		var message = "Data integrity violation: " + ex.getMostSpecificCause();
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ValidationError> handleDBValidationException(DataIntegrityViolationException ex) {
 		
-		return ResponseEntity.badRequest().body(message);
+		var message = "Data integrity violation: " + ex.getMostSpecificCause().getMessage();
+
+		var errors = List.of("database: " + message);
+
+		return ResponseEntity.badRequest().body(buildValidationError(errors, "Database validation failed."));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> handleValidationErrors(MethodArgumentNotValidException ex) {
+		
+		var errors = ex.getBindingResult().getFieldErrors().stream()
+				.map(error -> error.getField() + ": " +
+						(isBlank(error.getDefaultMessage()) ? "" : error.getDefaultMessage()))
+				.toList();
+
+		return ResponseEntity.badRequest().body(buildValidationError(errors, "Argument validation failed."));
+	}
+
+
+	private static ValidationError buildValidationError(List<String> errors, String message) {
+		
+		var validationError = new ValidationError();
+		validationError.setErrors(errors);
+		validationError.setMessage(message);
+
+		return validationError;
 	}
 }
